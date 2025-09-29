69e92a3353f09a16b5cb6246430f3bb3de49f6d4
Abby Ferguson <AbbyNicole9793@gmail.com>
2025-09-29 03:52:49 -0400

    project 12 attempt 1-4

--- utils/errors.js
+++ utils/errors.js
@@ -1,13 +1,7 @@
-const error500 = (res, err = {}) => {
-  return res.status(500).send({message: err.message})
-}
+const error500 = (res, err = {}) => res.status(500).send({message: err.message})
 
-const error400 = (res, err = {}) => {
-  return res.status(400).send({message: err.message})
-}
+const error400 = (res, err = {}) => res.status(400).send({message: err.message})
 
-const error404 = (res, err = {}) => {
-  return res.status(404).send({message: err.message})
-}
+const error404 = (res, err = {}) => res.status(404).send({message: err.message})
 
 module.exports = {error400, error404, error500}
