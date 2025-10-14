1d9e684a8fddd4633747372b1302566c677b0c63
Abby Ferguson <AbbyNicole9793@gmail.com>
2025-10-13 20:08:19 -0400

    project 13 attempt 3-9

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 const Item = require("../models/clothingItem")
-const {BAD_REQUEST, NOT_FOUND, SERVER_ERROR, UNAUTHORIZED_USER} = require("../utils/errors")
+const { BAD_REQUEST, NOT_FOUND, SERVER_ERROR, UNAUTHORIZED_USER } = require("../utils/errors")
 
 
 const getItems = (req, res) => {
@@ -17,9 +17,13 @@ const getItems = (req, res) => {
 const createItems = (req, res) => {
   console.log(req.user._id)
   const { name, weather, imageUrl } = req.body
-  const userId = req.user._id
+  const userId = req.user?._id
 
-  Item.create({name, weather, imageUrl, owner: userId})
+  if (!userId) {
+    return res.status(UNAUTHORIZED_USER).send({ message: "An error has occurred on the server" })
+  }
+
+  Item.create({ name, weather, imageUrl, owner: userId })
     .then((item) => res.status(201).send(item))
     .catch((err) => {
       console.error(err)
@@ -34,29 +38,28 @@ const deleteItems = (req, res) => {
   const { itemId } = req.params
   const userId = req.user._id
 
- if (!mongoose.Types.ObjectId.isValid(itemId)) {
+  if (!mongoose.Types.ObjectId.isValid(itemId)) {
     return res.status(BAD_REQUEST).send({ message: "Invalid item ID format" });
   }
 
   Item.findById(itemId)
-  .orFail()
-  .then((item) => {
-    if (item.owner.toString() !== userId) {
-      return res.status(UNAUTHORIZED_USER).send({ message: "You are not authorized to delete this item"})
-    }
-
-    return Item.deleteOne({ _id: itemId })
-      .then(() => res.status(200).send({message: "Item deleted successfuly"}))
-      .catch((err) => {
-       console.error(err)
-       if (err instanceof mongoose.Error.DocumentNotFoundError) {
-          return res.status(NOT_FOUND).send({ message: "An error has occurred on the server" })
-        }
-        if (err.name === "CastError") {
-           return res.status(BAD_REQUEST).send({ message: "An error has occurred on the server" })
-          }
-         return res.status(SERVER_ERROR).send({ message: "An error has occurred on the server" })
-      })
+    .orFail()
+    .then((item) => {
+      if (item.owner.toString() !== userId) {
+        return res.status(UNAUTHORIZED_USER).send({ message: "You are not authorized to delete this item" })
+      }
+      return Item.deleteOne({ _id: itemId })
+    .then(() => res.status(200).send({ message: "Item deleted successfuly" }))
+    })
+    .catch((err) => {
+      console.error(err)
+      if (err instanceof mongoose.Error.DocumentNotFoundError) {
+        return res.status(NOT_FOUND).send({ message: "An error has occurred on the server" })
+      }
+      if (err.name === "CastError") {
+        return res.status(BAD_REQUEST).send({ message: "An error has occurred on the server" })
+      }
+      return res.status(SERVER_ERROR).send({ message: "An error has occurred on the server" })
     })
 }
 
@@ -66,17 +69,17 @@ const likeItem = (req, res) => Item.findByIdAndUpdate(
   { new: true },
 )
   .orFail()
-    .then((item) => res.send(item))
-    .catch((err) => {
-      console.error(err);
-      if (err instanceof mongoose.Error.DocumentNotFoundError) {
-        return res.status(NOT_FOUND).send({ message: "An error has occurred on the server" })
-      }
-      if (err.name === "CastError") {
-        return res.status(BAD_REQUEST).send({ message: "An error has occurred on the server" })
-      }
-      return res.status(SERVER_ERROR).send({ message: "An error has occurred on the server" })
-    });
+  .then((item) => res.send(item))
+  .catch((err) => {
+    console.error(err);
+    if (err instanceof mongoose.Error.DocumentNotFoundError) {
+      return res.status(NOT_FOUND).send({ message: "An error has occurred on the server" })
+    }
+    if (err.name === "CastError") {
+      return res.status(BAD_REQUEST).send({ message: "An error has occurred on the server" })
+    }
+    return res.status(SERVER_ERROR).send({ message: "An error has occurred on the server" })
+  });
 
 
 const dislikeItem = (req, res) => Item.findByIdAndUpdate(
@@ -85,17 +88,17 @@ const dislikeItem = (req, res) => Item.findByIdAndUpdate(
   { new: true },
 )
   .orFail()
-    .then((item) => res.send(item))
-    .catch((err) => {
-      console.error(err);
-      if (err instanceof mongoose.Error.DocumentNotFoundError) {
-        return res.status(NOT_FOUND).send({ message: "An error has occurred on the server" })
-      }
-      if (err.name === "CastError") {
-       return res.status(BAD_REQUEST).send({ message: "An error has occurred on the server" })
-      }
-      return res.status(SERVER_ERROR).send({ message: "An error has occurred on the server" })
-    });
+  .then((item) => res.send(item))
+  .catch((err) => {
+    console.error(err);
+    if (err instanceof mongoose.Error.DocumentNotFoundError) {
+      return res.status(NOT_FOUND).send({ message: "An error has occurred on the server" })
+    }
+    if (err.name === "CastError") {
+      return res.status(BAD_REQUEST).send({ message: "An error has occurred on the server" })
+    }
+    return res.status(SERVER_ERROR).send({ message: "An error has occurred on the server" })
+  });
 
 
-module.exports = {getItems, createItems, deleteItems, likeItem, dislikeItem}
+module.exports = { getItems, createItems, deleteItems, likeItem, dislikeItem }
